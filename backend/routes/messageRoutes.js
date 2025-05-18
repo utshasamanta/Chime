d3db6ffa1b55aa9a07152139e263e97b4a30c4f0
utshasamanta <66881979+utshasamanta@users.noreply.github.com>
2025-05-18 17:46:21 -0600

    Delete backend directory

--- backend/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { sendMessage, getMessage } = require("../controllers/messageControllers.js")
-const isLoggedIn = require("../middleware/isLoggedIn.js");
-
-router.get("/:receiverId", isLoggedIn, getMessage);
-router.post("/:receiverId", isLoggedIn, sendMessage)
-
-module.exports = router;
