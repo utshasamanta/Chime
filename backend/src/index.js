53d076308f9dcebf18bdedb3f81255ce188fa439
Utsha Samanta <utshalm@gmail.com>
2025-06-05 02:18:07 -0600

    ready to deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 
 import { connectDB } from "./lib/db.js";
@@ -11,6 +12,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 
 app.use(express.json()); //lets us get the req body as a json
@@ -21,13 +23,16 @@ app.use(cors({
 }));
 
 
-
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-});
-
 app.use("/auth", authRoute);
 app.use("/messages", messageRoute);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`Chime Listening on Port ${PORT}`);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import User from "../../../backend/src/models/userModel";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
